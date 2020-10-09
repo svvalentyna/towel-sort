598d40d7f138baf27d99fd63eeda8682da0a6f57
Valentyna <svvalentyna@gmail.com>
2020-10-09 15:15:32 +0300

    feat: task towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
 // You should implement your task here.
 
+const { major } = require("semver");
+
 module.exports = function towelSort (matrix) {
-  return [];
+  let newArray = [];
+  if(matrix){
+    for(let i=0; i<matrix.length; i++){
+      if(i%2 == 0){
+        for(let j = 0; j < matrix[i].length; j++){
+          newArray.push(matrix[i][j])
+        }
+      } else {
+        for(let j = matrix[i].length - 1; j >= 0; j--){
+          newArray.push(matrix[i][j])
+        }
+      }
+    }
+  }
+  return newArray;
 }
